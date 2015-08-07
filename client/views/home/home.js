d1f1317d0975ea9431f327fe6badcd958f6487de
Mike Gaughan <michael.t.gaughan@gmail.com>
2015-08-06 23:51:58 -0600

    Initial commit - bangular generator defaults

---
+++ client/views/home/home.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('ngExample')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/home/home.html',
+        controller: 'HomeCtrl',
+        controllerAs: 'vm'
+      });
+  });
